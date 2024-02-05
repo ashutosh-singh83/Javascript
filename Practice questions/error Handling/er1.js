522c4369c283884d5c97525bbb868efaebbf1628
Ashutosh Singh <113412956+ashutosh-singh83@users.noreply.github.com>
2024-02-05 19:26:12 +0530

    Error Handling

---
+++ Practice questions/error Handling/er1.js
@@ -0,0 +1,20 @@
+let a=5;
+let b=10;
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+try{
+    console.log("a+b=",a+c);
+}
+catch(err){
+    console.log(err);
+}
+
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
+console.log("a+b=",a+b);
