98c5c7b91b9e233e889b149146c708fd9fbcda5e
Ashutosh Singh <113412956+ashutosh-singh83@users.noreply.github.com>
2024-02-10 10:26:44 +0530

    API

---
+++ Practice questions/Fetch API/api.js
@@ -0,0 +1,10 @@
+const URL="https://cat-fact.herokuapp.com/facts";
+const factPara=document.querySelector("#fact");
+const getFacts=async()=>{
+    console.log("getting data.......");
+    let response=await fetch(URL);
+    console.log(response);//JSON format
+    let data=await response.json();
+   factPara.innerText=data[0].text;
+}
+
